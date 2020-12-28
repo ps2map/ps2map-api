065c3efcf45b5e4f377f5c6ce9fcda835bc3850b
Leonhard S <leonhard-sei@outlook.com>
2020-12-28 18:36:45 +0100

    Add test getters

--- apl_api/_db/_getters.py
+++ apl_api/_db/_getters.py
@@ -1,11 +1,31 @@
 from typing import List
 
+import asyncpg
+
 from ..interfaces import ContinentInfo, ServerInfo
 
 
-async def get_continents() -> List[ContinentInfo]:
-    return []
+async def get_continents(pool: asyncpg.pool.Pool) -> List[ContinentInfo]:
+    conn: asyncpg.Connection
+    async with pool.acquire() as conn:  # type: ignore
+        rows = await conn.fetch(  # type: ignore
+            '''--sql
+            SELECT
+                ("id", "name")
+            FROM
+                "autopl"."Continent"
+            ;''')
+    return [ContinentInfo(*r) for r in rows]  # type: ignore
 
 
-async def get_servers() -> List[ServerInfo]:
-    return []
+async def get_servers(pool: asyncpg.pool.Pool) -> List[ServerInfo]:
+    conn: asyncpg.Connection
+    async with pool.acquire() as conn:  # type: ignore
+        rows = await conn.fetch(  # type: ignore
+            '''--sql
+            SELECT
+                ("id", "name", "region")
+            FROM
+                "autopl"."Server"
+            ;''')
+    return [ServerInfo(*r) for r in rows]  # type: ignore
