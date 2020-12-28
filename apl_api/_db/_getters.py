7b5be72d38530d6a480055b14cd3df4408f429c7
Leonhard S <leonhard-sei@outlook.com>
2020-12-28 17:37:29 +0100

    Add dummy database getters
    These will be replaced with actual database interfaces later.

---
+++ apl_api/_db/_getters.py
@@ -0,0 +1,11 @@
+from typing import List
+
+from ..interfaces import ContinentInfo, ServerInfo
+
+
+async def get_continents() -> List[ContinentInfo]:
+    return []
+
+
+async def get_servers() -> List[ServerInfo]:
+    return []
