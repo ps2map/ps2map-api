23dda3198e106d574ff8fad0127e34275f1487aa
Leonhard S <leonhard-sei@outlook.com>
2020-12-28 17:37:29 +0100

    Add dummy database getters
    These will be replaced with actual database interfaces later.

---
+++ apl_api/_db/__init__.py
@@ -0,0 +1,14 @@
+"""Database abstraction module for the API host.
+
+This module exposes asynchronous getter methods to use in place of
+actual database interactions. This both allows changes to the database
+without interfering with the API, and it also provides a convenient
+hook to add caching and other optimisations down the line
+"""
+
+from ._getters import get_continents, get_servers
+
+__all__ = [
+    'get_continents',
+    'get_servers'
+]
