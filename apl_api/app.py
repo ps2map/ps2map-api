2b0796d28589d648fe686107d8b779c32b7ef93f
Leonhard S <leonhard-sei@outlook.com>
2020-12-29 19:40:26 +0100

    Fix logging behaviour
    The logging adjustments must be carried out in the server's thread to work.

--- apl_api/app.py
+++ apl_api/app.py
@@ -7,13 +7,21 @@ None of the contents of this module are meant to be exported or used in
 other modules; this is the main client.
 """
 
+import logging
+
 import fastapi
 
 from . import endpoints
+from ._logging import ForwardHandler
 
 # NOTE: The fragmentation of the endpoints is mostly to simplify adaptions, it
 # has a neglegible performance impact upon startup and is just as speedy as a
 # monolithic API application once it's set up and running.
+
+# Customise logging behaviour
+_api_log = logging.getLogger('api.server')
+_uvicorn_log = logging.getLogger('uvicorn')
+_uvicorn_log.handlers = [ForwardHandler(_api_log)]
 
 # Create the API application
 app = fastapi.FastAPI()
