89305d2bb8a345bf83b862d5d5f3486131519dca
Leonhard S <leonhard-sei@outlook.com>
2020-12-29 19:10:43 +0100

    Move DB connection pool to dependency

--- apl_api/endpoints/continents.py
+++ apl_api/endpoints/continents.py
@@ -1,5 +1,6 @@
 """API endpoints for PS2 continents/zones."""
 
+import asyncpg
 import fastapi
 from starlette.responses import JSONResponse
 
@@ -7,8 +8,10 @@ from .. import _db as database
 
 router = fastapi.APIRouter(prefix='/continents')
 
+_dep_pool = fastapi.Depends(database.get_pool)  # type: ignore
+
 
 @router.get('/')  # type: ignore
-async def get_continents() -> JSONResponse:
-    continent_list = await database.get_continents()
+async def get_continents(pool: asyncpg.pool.Pool = _dep_pool) -> JSONResponse:
+    continent_list = await database.get_continents(pool)
     return JSONResponse({'continent_list': continent_list})
