136692de689cf8be3cb0c5f5c422aebbc17d2ce9
Leonhard S <leonhard-sei@outlook.com>
2021-02-17 14:05:41 +0100

    Add hard-coded interface payloads

--- apl_api/endpoints/servers.py
+++ apl_api/endpoints/servers.py
@@ -5,15 +5,25 @@ import dataclasses
 import fastapi
 from starlette.responses import JSONResponse
 
-from .. import _db as database
+from ..interfaces import ServerInfo
+from ..types import FactionData
 
 router = fastapi.APIRouter(prefix='/servers')
 
-_dep_pool = fastapi.Depends(database.get_pool)  # type: ignore
-
 
 @router.get('/')  # type: ignore
-async def get_servers(pool: asyncpg.pool.Pool = _dep_pool) -> JSONResponse:
-    server_list = [dataclasses.asdict(d)
-                   for d in await database.get_servers(pool)]
+async def get_servers() -> JSONResponse:
+    servers = [
+        ServerInfo(1, 'Connery', 'high', 'US West',
+                   FactionData(28.0, 33.0, 34.0, 5.0)),
+        ServerInfo(10, 'Miller', 'high', 'EU',
+                   FactionData(33.0, 34.0, 32.0, 1.0)),
+        ServerInfo(13, 'Cobalt', 'medium', 'EU',
+                   FactionData(34.0, 33.0, 33.0, 0.0)),
+        ServerInfo(17, 'Emerald', 'medium', 'US East',
+                   FactionData(39.0, 35.0, 30.0, 2.0)),
+        ServerInfo(40, 'SolTech', 'low', 'Asia',
+                   FactionData(32.0, 34.0, 31.0, 3.0))
+    ]
+    server_list = [dataclasses.asdict(d) for d in servers]
     return JSONResponse({'server_list': server_list})
