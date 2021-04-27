cba3b42e38a1401d3cc26bf8893b54f7d227b91b
Leonhard S <leonhard-sei@outlook.com>
2021-04-27 15:34:29 +0200

    Add CORS middleware to appease JS

--- apl_api/app.py
+++ apl_api/app.py
@@ -10,13 +10,15 @@ other modules; this is the main client.
 import logging
 
 import fastapi
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import endpoints
 from ._logging import ForwardHandler
 
-# NOTE: The fragmentation of the endpoints is mostly to simplify adaptions, it
-# has a neglegible performance impact upon startup and is just as speedy as a
-# monolithic API application once it's set up and running.
+# List of remote hosts for which CORS reponses headers should be included
+_ORIGINS = [
+    '*'
+]
 
 # Customise logging behaviour
 _api_log = logging.getLogger('api.server')
@@ -25,6 +27,15 @@ _uvicorn_log.handlers = [ForwardHandler(_api_log)]
 
 # Create the API application
 app = fastapi.FastAPI()
+
+# Add CORS middleware to inject appropriate response headers
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=_ORIGINS)
+
+# NOTE: The fragmentation of the endpoints is mostly to simplify adaptions, it
+# has a neglegible performance impact upon startup and is just as speedy as a
+# monolithic API application once it's set up and running.
 
 # Register the different path routers for the various endpoints
 for name in endpoints.__all__:
