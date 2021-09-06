14a17cdea0ce1ae7612eb881562a4ddf9164714b
Leonhard S <leonhard-sei@outlook.com>
2021-09-06 23:01:02 +0200

    Add map tile extractor CLI
    Map merging is currently broken for maps that are not 8192 px in size.

--- tools/README.md
+++ tools/README.md
@@ -26,3 +26,30 @@ Alternatively, the module can also be imported and targeted from other Python mo
 
 For detailed usage instructions for these methods, please refer to the
 corresponding docstrings in the source.
+
+## Map Tile Extractor
+
+A Python script for extracting PlanetSide 2 map tile textures.
+
+Note that this utility requires a local installation of PlanetSide 2 to work. By default, it will search the following common directories for PS2 installations:
+
+- C:\Users\Public\Sony Online Entertainment\Installed Games\PlanetSide 2
+- C:\Program Files (x86)\Steam\steamapps\common\PlanetSide 2
+- C:\Program Files\PlanetSide 2
+- D:\Steam\steamapps\common\PlanetSide 2
+- D:\PlanetSide 2
+
+If the PS2 install on the target device does not match any of them, you can specify an alternate installation using the `--dir <dir>` switch.
+
+This utility requires [Pillow](https://python-pillow.org/) version 8.3 or greater.
+
+### Formats
+
+This script converts three modes:
+
+- **raw**: Only extract the map tiles, leaving them as 256 px tiles in DDS format.
+- **convert**: Extract the map tiles and export them as 256 px tiles in PNG format.
+- **apl**: Export the map tiles and save them as 1024 px JPEG images using the APL naming scheme.
+- **merge**: Merge the map tiles into a single large PNG image.
+
+For detailed information on the command line switches available, please refer to the script's help (invoked via the `--help` switch).
--- tools/map_tile_extractor.py
+++ tools/map_tile_extractor.py
@@ -27,14 +27,17 @@ own risk.
 # @RhettVX's "fl_name_scrape" utility in his Forgelight Toolbox:
 # <https://github.com/RhettVX/forgelight-toolbox>
 
+import argparse
+import dataclasses
+import math
 import os
 import pathlib
 import re
 import sys
 import tempfile
-from typing import Iterable, List, Optional, Set
+from typing import Dict, Iterable, List, Optional, Set
 
-from DbgPack import AssetManager
+from DbgPack import AssetManager  # type: ignore
 from PIL import Image
 
 # Regular expression used to select the files to scrape for map tiles
@@ -51,10 +54,40 @@ PS2_INSTALL_DIRECTORIES = [
     r'C:\Users\Public\Sony Online Entertainment\Installed Games\PlanetSide 2',
     r'C:\Program Files (x86)\Steam\steamapps\common\PlanetSide 2',
     r'C:\Program Files\PlanetSide 2',
-    r'D:\Steam\steamapps\common\PlanetSide 2'
+    r'D:\Steam\steamapps\common\PlanetSide 2',
+    r'D:\PlanetSide 2'
 ]
 # Name of the PS2 executable, used to confirm installation folders
 PS2_EXCUTABLE_NAME = 'PlanetSide2_x64.exe'
+
+# Base size of in-game map tiles
+PS2_TILE_SIZE = 256
+APL_TILE_SIZE = 1024
+
+# Type aliases
+_LodTileMap = Dict[int, List['_MapTile']]
+
+
+@dataclasses.dataclass()
+class _MapTile:
+    """Helper object containing map tile data."""
+
+    # pylint: disable=invalid-name
+
+    u: int
+    v: int
+    lod: int
+    path: pathlib.Path
+
+
+@dataclasses.dataclass()
+class _TileMinMax:
+    """Helper object storing min/max tile coordinates."""
+
+    min_u: int
+    max_U: int
+    min_v: int
+    max_V: int
 
 
 def _bytesToString(bytes_: bytes) -> str:
@@ -149,51 +182,6 @@ def _get_tile_namelist(paths: Iterable[pathlib.Path]) -> List[str]:
     return sorted(namelist)
 
 
-def _convert_image(path: pathlib.Path,
-                   output_dir: Optional[pathlib.Path] = None,
-                   format_: str = 'png') -> None:
-    """Convert a DDS texture asset into a new format.
-
-    By default, this convertex the texture to PNG format. You can
-    specify alternate formats via the `format` field. This field is
-    used as the file extension when writing the image using pillow
-    (<https://pillow.readthedocs.io/en/stable/>). You may specify any
-    extension supported by pillow.
-
-    If no output directory is specified, the converted images will be
-    placed in the same directory as the source files. Input files must
-    be in DDS format and are required to use the "dds" extension (case
-    insensitive).
-
-    Args:
-        path (pathlib.Path): Path to the input image to read
-        output_dir (Optional[pathlib.Path]): Directory to write the
-            converted images to
-        format_ (str): File extension of the image format to convert the
-            files to, see the pillow docs for supported extensions
-
-    Raises:
-        RuntimeError: Raised if the given input file is not in DDS
-            format or if the `format_` given is not supported
-
-    """
-    if os.path.splitext(str(path))[1].lower() != '.dds':
-        raise RuntimeError(f'Input file is not in DDS format: {path}')
-    # If no output directory was provided, use the input file's directory
-    if output_dir is None:
-        print('output dir is none')
-        output_dir = pathlib.Path(os.path.dirname(path))
-    if not os.path.isdir(output_dir):
-        raise FileNotFoundError(f'Path is not a directory: {output_dir}')
-    # Convert the image
-    out_path = output_dir / f'{os.path.basename(path)}.{format_}'
-    image = Image.open(path)
-    try:
-        image.save(out_path)
-    except ValueError as err:
-        raise RuntimeError(f'Unknown file format: {format_}') from err
-
-
 def _unpack_files(manager: AssetManager, output_dir: pathlib.Path) -> None:
     """Unpack select assets from a *.pack2 archive.
 
@@ -216,21 +204,195 @@ def _unpack_files(manager: AssetManager, output_dir: pathlib.Path) -> None:
         (output_dir / asset.name).write_bytes(asset_data)
 
 
-if __name__ == '__main__':
-    output_dir = pathlib.Path('./unpack')
-    if not output_dir.exists():
-        os.makedirs(output_dir)
+def _validate_map_tiles(tile_map: _LodTileMap) -> bool:
+    """Validate a collection of map tiles.
 
-    format_ = 'png'
-    # TODO: Add --dir switch to allow overriding search locations
-    custom_dir = ''
+    This tool ensures the tiles span a valid, square grid without any
+    holes.
+
+    Args:
+        tile_map (Dict[int, List[_MapTile]]): A collection of map tiles
+            to validate
+
+    Returns:
+        bool: true if the tiles make up a valid map grid, otherwise
+            false
+
+    """
+    # Find extreme coordinate values for each map LOD
+    extremes: Dict[int, _TileMinMax] = {}
+    for lod, tile_list in tile_map.items():
+        for tile in tile_list:
+            # If the current LOD is not yet stored in the dict, create a dict
+            # of initial values
+            if not lod in extremes:
+                extremes[lod] = _TileMinMax(tile.u, tile.u, tile.v, tile.v)
+                continue
+            lod_extremes = extremes[lod]
+            # Update extreme values
+            if tile.u < lod_extremes.min_u:
+                lod_extremes.min_u = tile.u
+            elif tile.u > lod_extremes.max_U:
+                lod_extremes.max_U = tile.u
+            if tile.v < lod_extremes.min_v:
+                lod_extremes.min_v = tile.v
+            elif tile.v > lod_extremes.max_V:
+                lod_extremes.max_V = tile.v
+
+    # Validate that each LOD makes up a valid grid
+    for lod, lod_extremes in extremes.items():
+        # Calculate the step size used in-between tile coordinates
+        lod_step_size = 2 ** (lod+2)
+        # Ensure the grid is square
+        if (lod_extremes.min_u != lod_extremes.min_v
+                or lod_extremes.max_U != lod_extremes.max_V):
+            return False
+        # Create a grid of all tile coordinates to test
+        grid: Dict[int, Dict[int, bool]] = {}
+        min_, max_ = lod_extremes.min_u, lod_extremes.max_U
+        for u in range(min_, max_ + 1, lod_step_size):
+            grid[u] = {}
+            for v in range(min_, max_ + 1, lod_step_size):
+                grid[u][v] = False
+        # Update the grid with all coordinates from the map tiles
+        for tile in (t for t in tile_map[lod] if t.lod == lod):
+            grid[tile.u][tile.v] = True
+        # Check the test grid and ensure every position was hit
+        for u in range(min_, max_ + 1, lod_step_size):
+            for v in range(min_, max_ + 1, lod_step_size):
+                if not grid[u][v]:
+                    return False
+    return True
+
+
+def _group_tiles(temp_path: pathlib.Path) -> Dict[str, _LodTileMap]:
+    """Group map tile assets by map name.
+
+    Args:
+        temp_path (pathlib.Path): Directory containing the map tiles
+
+    Returns:
+        Dict[str, Dict[int, List[_MapTile]]]: A mapping of nested
+            dictionaries. The outer dict key represents the name of the
+            map, the inner dictionary groups the map's tiles by LOD
+            level.
+
+    """
+    map_tiles: Dict[str, Dict[int, List[_MapTile]]] = {}
+    for filename in os.listdir(temp_path):
+        # Extract tile properties from the asset's filename
+        name, _, coord_u, coord_v, lod_str = filename.split('.')[0].split('_')
+        lod = int(lod_str[-1])
+        tile = _MapTile(int(coord_u), int(coord_v), lod, temp_path / filename)
+        # Get the dictionary containing the map tiles
+        try:
+            lod_tiles = map_tiles[name]
+        except KeyError:
+            lod_tiles = {}
+            map_tiles[name] = lod_tiles
+        # Add the tile to the list of tiles for this LOD level
+        try:
+            lod_tiles[lod].append(tile)
+        except KeyError:
+            lod_tiles[lod] = [tile]
+    return map_tiles
+
+
+def _merge_tiles(temp_path: pathlib.Path) -> Dict[str, Dict[int, Image.Image]]:
+    """Merge all tiles.
+
+    Args:
+        temp_path (pathlib.Path): Directory containing the map tiles
+
+    Returns:
+        Dict[str, Dict[int, Image.Image]]: A mapping of nested
+            dictionaries. The outer dict key represents the name of the
+            map, the inner dictionary groups the map's tiles by LOD
+            level.
+    """
+    print(' >> Grouping map tiles...')
+    grouped: Dict[str, _LodTileMap] = _group_tiles(temp_path)
+    # Generate merged tiles
+    output: Dict[str, Dict[int, Image.Image]] = {}
+    for name, lod_tiles in grouped.items():
+        print(f' >> Merging map tiles for "{name}"...')
+        output[name] = {}
+        _validate_map_tiles(lod_tiles)
+        for lod, tiles in lod_tiles.items():
+            merged_size = int(math.sqrt(len(tiles))) * PS2_TILE_SIZE
+            img = Image.new('RGB', (merged_size, merged_size))
+            for tile in tiles:
+                tile_img = Image.open(tile.path)
+                tile_u = int((64 + tile.u) / (2 ** (lod+2))) * PS2_TILE_SIZE
+                tile_v = int((64 + tile.v) / (2 ** (lod+2))) * PS2_TILE_SIZE
+                img.paste(tile_img, (tile_u, tile_v))
+            output[name][lod] = img.transpose(Image.FLIP_TOP_BOTTOM)
+    return output
+
+
+def _process_raw(temp_path: pathlib.Path, out_path: pathlib.Path) -> None:
+    """Script handler for "raw" format."""
+    for filename in os.listdir(temp_path):
+        os.rename(temp_path / filename, out_path / filename)
+
+
+def _process_convert(temp_path: pathlib.Path, out_path: pathlib.Path) -> None:
+    """Script handler for "convert" format."""
+    files = os.listdir(temp_path)
+    total = len(files)
+    for index, filename in enumerate(files):
+        print(f' >> Converting file {index+1} of {total}')
+        outname = filename.rsplit('.', maxsplit=1)[0] + '.png'
+        img = Image.open(temp_path / filename)
+        img = img.transpose(Image.FLIP_TOP_BOTTOM)
+        img.save(out_path / outname)
+
+
+def _process_apl(temp_path: pathlib.Path, out_path: pathlib.Path) -> None:
+    """Script handler for "apl" format."""
+    images_merged = _merge_tiles(temp_path)
+    for name, lod_images in images_merged.items():
+        for lod, image in lod_images.items():
+            assert image.height == image.width
+            # Calculate the number of slices
+            for start_x in range(0, image.height, APL_TILE_SIZE):
+                end_x = start_x + APL_TILE_SIZE
+                tile_x = start_x / APL_TILE_SIZE - 4
+                if start_x >= image.height / 2:
+                    tile_x += 1
+                for start_y in range(0, image.height, APL_TILE_SIZE):
+                    end_y = start_y + APL_TILE_SIZE
+                    tile_y = start_y / APL_TILE_SIZE - 4
+                    if start_x >= image.height / 2:
+                        tile_y += 1
+                    img_apl = image.crop((start_x, start_y, end_x, end_y))
+                    outname = f'{name}_lod{lod}_{tile_x}_{tile_y}.jpg'
+                    print(f' >> Creating tile {outname}')
+                    img_apl.save(out_path / outname, quality=95, sampling=0)
+
+
+def _process_merge(temp_path: pathlib.Path, out_path: pathlib.Path) -> None:
+    """Script handler for "merge" format."""
+    img_merged: Dict[str, Dict[int, Image.Image]] = _merge_tiles(temp_path)
+    for name, map_tiles in img_merged.items():
+        for lod, image in map_tiles.items():
+            filename = f'{name}_LOD{lod}.png'
+            image.save(out_path / filename)
+
+
+def main(format: str, dir: Optional[str], output: str, namelist: bool) -> None:
+    """Main script for tile extraction."""
+    # Create the output directory if it does not exist yet
+    out_path = pathlib.Path(output)
+    if not out_path.exists():
+        os.makedirs(out_path)
 
     # Locate the PS2 installation and asset folder
     print('\nLocating PlanetSide 2 install directory...')
-    if not custom_dir:
+    if dir is None:
         print(' >> No directory specified, searching common install paths')
     else:
-        print(f' >> Using provided installation directory at "{custom_dir}"')
+        print(f' >> Using provided installation directory at "{dir}"')
     ps2_dir = _find_game_folder()
     print(' >> PlanetSide 2 executable found')
     asset_dir = ps2_dir / 'Resources' / 'Assets'
@@ -242,11 +404,14 @@ if __name__ == '__main__':
     data_files = [asset_dir / f for f in os.listdir(asset_dir)
                   if file_regex_pattern.fullmatch(f)]
     print(f' >> Scraping {len(data_files)} archive/s for matching names...')
-    namelist = _get_tile_namelist(data_files)
-    print(f' >> done\n >> {len(namelist)} matching assets found')
-    if not namelist:
+    names = _get_tile_namelist(data_files)
+    print(f' >> done\n >> {len(names)} matching assets found')
+    if not names:
         print('Unable to extract files due to empty namelist')
         sys.exit(1)
+    if namelist:
+        with open(out_path / 'namelist.txt', 'w') as namelist_file:
+            namelist_file.writelines((f'{s}\n' for s in names))
 
     # Unpack all assets in a temporary directory
     print('\nExtracting assets...')
@@ -261,21 +426,44 @@ if __name__ == '__main__':
               f' >> Selected {total} archives\n')
         for index, archive in enumerate(archives):
             print(f' >> Extracting archive {index+1} of {total}: {archive}')
-            manager = AssetManager([asset_dir / archive], namelist=namelist)
+            manager = AssetManager([asset_dir / archive], namelist=names)
             _unpack_files(manager, temp_path)
 
-        # Recombine the small assets into larger blocks
-        print('\nMerge tile assets\n >> Not yet implemented, skipping...')
-        # TODO: Implement re-merging of map tiles
+        print(f'\n >> Processing files using format "{format}"...')
 
-        # Convert DDS textures into a more friendly format
-        if format_ is not None:
-            print('\nConverting DDS textures...\n'
-                  f' >> Using export format "{format_}"\n')
-            images = [pathlib.Path(f) for f in os.listdir(temp_path)]
-            total = len(images)
-            for index, image in enumerate(images):
-                print(f' >> Converting asset {index+1} of {total}: '
-                      f'{image}')
-                _convert_image(temp_path / image, output_dir, format_)
-            print('\n >> done\n')
+        # Recombine the small assets into larger blocks
+        if format == 'raw':
+            _process_raw(temp_path, out_path)
+        elif format == 'convert':
+            _process_convert(temp_path, out_path)
+        elif format == 'apl':
+            _process_apl(temp_path, out_path)
+        elif format == 'merge':
+            _process_merge(temp_path, out_path)
+        else:
+            raise RuntimeError(f'Unhandled format: {format}')
+
+        print('\ndone\n')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'format', default='convert', nargs='?',
+        choices=['raw', 'convert', 'apl', 'merge'],
+        help='The export format to use. raw: export files in 256 px DDS. '
+        'convert: flip files and export them as 256 px PNGs. apl: '
+        'export files in 1024 px JPEG. merge: merge all tiles into a '
+        'single large PNG image.')
+    parser.add_argument(
+        '--dir', '-d', nargs=1, default=None, type=str,
+        help='The directory containing the PlanetSide 2 executable.')
+    parser.add_argument(
+        '--output', '-o', nargs=1, default='./map_assets', type=str,
+        help='Output directory to save the exported files to.')
+    parser.add_argument(
+        '--namelist', '-n', action='store_true',
+        help='If set, the full namelist will be exported.')
+    kwargs = vars(parser.parse_args())
+
+    main(**kwargs)
