e6cc50de8a57072ebca48c12507fe7db6ad287cb
Leonhard S <leonhard-sei@outlook.com>
2022-05-16 18:29:07 +0200

    Remove static testing data

--- server/routes/continents.py
+++ server/routes/continents.py
@@ -1,24 +1,12 @@
 """API routes for PS2 continents/zones."""
 
-import datetime
-import random
-from typing import cast
-
 import fastapi
 from fastapi.params import Query
-from pydantic.types import PositiveInt
 
 from ..interfaces import ContinentInfo, ContinentStatus
-from ..types import FactionId, Population, ServerId
-from ._utils import static_from_json
-
-# HACK: Load static development data
-from .servers import _STATIC_SERVER_DATA as SERVERS  # type: ignore
+from ..types import ServerId
 
 router = fastapi.APIRouter(prefix='/continents')
-
-_STATIC_CONTINENT_DATA = static_from_json(
-    ContinentInfo, 'static_continents.json')
 
 
 @router.get('/info', response_model=list[ContinentInfo])
@@ -29,12 +17,12 @@ async def continent_info() -> list[ContinentInfo]:
     API consumers are expected to aggressively cache the returned data
     as they will only change with major game updates.
     """
-    return list(_STATIC_CONTINENT_DATA.values())
+    return []
 
 
 @router.get('/status', response_model=list[ContinentStatus])
 async def continent_status(
-    server_id: PositiveInt = Query(  # type: ignore
+    server_id: ServerId = Query(  # type: ignore
         ...,
         title='Server ID',
         description='Unique identifier of the server for which to return a '
@@ -46,46 +34,5 @@ async def continent_status(
     This endpoint will likely be moved to or replicated in a WebSocket
     endpoint in future versions.
     """
-    if server_id not in SERVERS:
-        msg = f'Unknown server ID: {server_id}'
-        raise fastapi.HTTPException(status_code=404, detail=msg)
     data: list[ContinentStatus] = []
-    for continent in _STATIC_CONTINENT_DATA.values():
-        status = 'locked' if random.random() < 0.6 else 'open'
-        locked = bool(status == 'locked')
-        if locked:
-            population = Population(
-                vs=0, nc=0, tr=0, nso=0)
-            locked_by = cast(FactionId, random.randint(1, 3))
-            alert_active = False
-            alert_started = None
-            alert_ends = None
-        else:
-            population = Population(
-                vs=random.randint(50, 300),
-                nc=random.randint(50, 300),
-                tr=random.randint(50, 300),
-                nso=random.randint(0, 10))
-            locked_by = None
-            alert_active = random.random() < 0.5
-            alert_started = None
-            alert_ends = None
-            if alert_active:
-                start = (
-                    datetime.datetime.now()
-                    - datetime.timedelta(minutes=random.randint(5, 80)))
-                alert_started = int(start.timestamp())
-                alert_ends = int(
-                    (start + datetime.timedelta(minutes=90)).timestamp())
-            print(continent)
-        data.append(
-            ContinentStatus(
-                id=continent.id,
-                server_id=cast(ServerId, server_id),
-                population=population,
-                status=status,
-                locked_by=locked_by,
-                alert_active=alert_active,
-                alert_started=alert_started,
-                alert_ends=alert_ends))
     return data
--- server/routes/servers.py
+++ server/routes/servers.py
@@ -1,18 +1,10 @@
 """API routes for PS2 game servers."""
 
-import random
-from typing import cast
-
 import fastapi
 
 from ..interfaces import ServerInfo, ServerStatus
-from ..types import ContinentId, Population
-from ._utils import static_from_json
-
 
 router = fastapi.APIRouter(prefix='/servers')
-
-_STATIC_SERVER_DATA = static_from_json(ServerInfo, 'static_servers.json')
 
 
 @router.get('/info', response_model=list[ServerInfo])
@@ -23,7 +15,7 @@ async def server_info() -> list[ServerInfo]:
     region. API consumers are expected to aggressively cache the
     returned data as they will only change with major game updates.
     """
-    return list(_STATIC_SERVER_DATA.values())
+    return []
 
 
 @router.get('/status', response_model=list[ServerStatus])
@@ -34,23 +26,4 @@ async def server_status() -> list[ServerStatus]:
     endpoint in future versions.
     """
     data: list[ServerStatus] = []
-    for server in _STATIC_SERVER_DATA.values():
-        # Make up random data
-        status = 'online' if random.random() < 0.9 else 'locked'
-        base_pop = random.randint(10, 300)
-        population = Population(
-            vs=base_pop + random.randint(0, 100),
-            nc=base_pop + random.randint(0, 100),
-            tr=base_pop + random.randint(0, 100),
-            nso=int(base_pop*0.05))
-        continents = [cast(ContinentId, i)
-                      for i in (2, 4, 6, 8) if random.random() < 0.5]
-        if not continents:
-            continents.append(cast(ContinentId, 2))
-        data.append(
-            ServerStatus(
-                id=server.id,
-                status=status,
-                population=population,
-                open_continents=continents))
     return data
